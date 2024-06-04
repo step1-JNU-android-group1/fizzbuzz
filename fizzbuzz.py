51d361b39febc8530d3ad078dae952c55ae0d09a
tlsgptj <125953570+tlsgptj@users.noreply.github.com>
2024-06-04 20:39:05 +0900

    Colab을 통해 생성됨

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,55 @@
-for i in range(1, 15+1):
-	if i % 3 == 0:
-		print('fizz')
-	if i % 5 == 0:
-		print('buzz')
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "private_outputs": true,
+      "provenance": [],
+      "authorship_tag": "ABX9TyMkgYRQ/0Njuep7LOwEBMcM",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/tlsgptj/fizzbuzz/blob/main/fizzbuzz.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "2sbUzGrp7s_B"
+      },
+      "outputs": [],
+      "source": [
+        "for i in range(1, 15):\n",
+        "    if i % 3 == 0 and i % 5 == 0:\n",
+        "        print(\"FizzBuzz\")\n",
+        "    else:\n",
+        "        print(i)"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "dTxL98tG7vPI"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
