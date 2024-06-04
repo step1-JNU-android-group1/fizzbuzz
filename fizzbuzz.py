e0bdff5c4b3a164a45cbbad06d5577f4f297918c
yb0x00 <yeongbeen0x00@gmail.com>
2024-06-04 21:27:29 +0900

    Merge Test

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,4 @@
 for i in range(1, 15+1):
-<<<<<<< HEAD
         if i % 3 == 0 or i % 5 == 0:
                     print('fizz' * (i % 3 == 0) + 'buzz' * (i % 5 == 0))
         else: print(i)
-=======
-	if i % 3 == 0:
-		print('fizz')
-	if i % 5 == 0:
-		print('buzz')
->>>>>>> b2f133f40d349919c652f9c0307332e9e442ce2b
